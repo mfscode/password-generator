fe91e8c817a3b1d502243ab56eaf180aacfec6df
Muhammad Fadil <fadiljayaputra@gmail.com>
2022-04-04 09:17:49 +0700

    Okelah, semua udah beres, tinggal bikin copy to clipboard

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ let pass = {
 let alphabet = 'qwertyuiopasdfghjklzxcvbnm'
 let lowerCase = alphabet.split(" ")
 let upperCase = alphabet.toUpperCase().split(" ")
+let symbols = '!@#$%&<>'
+let symbolsArr = symbols.split(" ")
 
-const available = Array.from(lowerCase + upperCase)
+const available = Array.from(lowerCase + upperCase + symbolsArr)
 
 let theValue = {
-    one: available[Math.floor(Math.random() * 52)],
-    two: available[Math.floor(Math.random() * 52)],
-    three: available[Math.floor(Math.random() * 52)],
-    four: available[Math.floor(Math.random() * 52)]
+    one: available[Math.floor(Math.random() * 60)],
+    two: available[Math.floor(Math.random() * 60)],
+    three: available[Math.floor(Math.random() * 60)],
+    four: available[Math.floor(Math.random() * 60)]
 }
 
 
@@ -32,10 +34,10 @@ function generate() {
     theValue.three = ""
     theValue.four = ""
     for (let i = 0; i < 8; i++) {
-        theValue.one += available[Math.floor(Math.random() * 52)];
-        theValue.two += available[Math.floor(Math.random() * 52)];
-        theValue.three += available[Math.floor(Math.random() * 52)];
-        theValue.four += available[Math.floor(Math.random() * 52)];
+        theValue.one += available[Math.floor(Math.random() * 60)];
+        theValue.two += available[Math.floor(Math.random() * 60)];
+        theValue.three += available[Math.floor(Math.random() * 60)];
+        theValue.four += available[Math.floor(Math.random() * 60)];
     }
     firstBtn.value = theValue.one;
     secondBtn.value = theValue.two;
@@ -43,7 +45,13 @@ function generate() {
     fourthBtn.value = theValue.four;
 }
 
+function copy1() {
+    var firstPw = document.getElementById("1")
+    firstPw.select()
+    navigator.clipboard.writeText(firstPw.value)
+}
 
 
 
-console.log(available)
+
+console.log(firstBtn.innerText)
